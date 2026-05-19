889d3345635d100a7837d07b8acc3a10efc8a7ad
Mangal Vanik <vanikmangal7@gmail.com>
2026-05-19 11:29:06 +0530

    fix: allow Vercel frontend URL in CORS

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,8 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:3000',
   'http://localhost:3001',
-  'http://127.0.0.1:3001'
+  'http://127.0.0.1:3001',
+  'https://propulse-fruntend.vercel.app'
 ];
 
 app.use(cors({
